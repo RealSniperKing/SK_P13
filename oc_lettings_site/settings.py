c11a56310ac43c90a708b7cff89a2b19cac9099b
Luc <luke.c@laposte.net>
2022-04-22 16:32:18 +0200

    Fix warnings :  (models.W042) Auto-created primary key used when not defining a primary key type, by default 'django.db.models.AutoField'.

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,5 +1,5 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -106,3 +106,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
