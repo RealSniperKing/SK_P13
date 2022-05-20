fe87e14675553030f36a4a0bebf21afa36f4d93b
Luc <luke.c@laposte.net>
2022-05-20 08:30:08 +0200

    tests

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,25 @@
+from django.urls import reverse
+from django.test import Client
+from bs4 import BeautifulSoup
+
+
 def test_dummy():
     assert 1
+
+
+def test_admin():
+    url = reverse('admin:index')
+    print("url =", url)
+    response = Client().get(url)
+    assert response.status_code == 302
+
+    login_url = response.url
+    print("login_url = ", login_url)
+
+    response = Client().get(login_url)
+    assert response.status_code == 200
+
+    content = response.content
+    soup = BeautifulSoup(content, "html.parser")
+    title = soup.find('h1')
+    assert title.text == "Django administration"
