ae74269caeee24674b115e44ade20e2d9bbec996
Luc <luke.c@laposte.net>
2022-06-09 10:02:49 +0200

    Add admin path

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,10 +1,15 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-
-from . import views
 
 urlpatterns = [
     path('', include("home.urls")),
     path('', include("lettings.urls")),
-    path('', include("profiles.urls"))
+    path('', include("profiles.urls")),
+    path('admin/', admin.site.urls)
 ]
+
+# if settings.DEBUG:
+#     urlpatterns += [
+#         path('__debug__/', include('debug_toolbar.urls')),
+#     ]
