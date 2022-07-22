548b4cf313ca8ea47153e655c99f6f482aa23a98
Luc <lchambaud01@gmail.com>
2022-07-22 10:58:19 +0200

    add trigger_error

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-
 from . import views
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 
 app_name = "home"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sentry-debug/', trigger_error),
 ]
